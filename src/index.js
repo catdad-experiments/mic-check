fd922c8a452be419934101d8d749fb69ade2e4b1
Kiril Vatev <vatev.1@gmail.com>
2017-06-06 20:33:49 -0400

    starting to add logic to capture the mic

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@
 
 window.addEventListener('load', function () {
   console.log('page has loaded');
+
+  var player = document.querySelector('#player');
+  var testBtn = document.querySelector('#test');
+
+  function startCapture(stream) {
+    console.log(stream);
+
+    if (window.URL) {
+      player.src = window.URL.createObjectURL(stream);
+    } else {
+      player.src = stream;
+    }
+  }
+
+  function permissionError(err) {
+    // TODO don't alert
+    console.error(err);
+  }
+
+  // TODO fall back to navigator.getUserMedia is necessary
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(startCapture)
+    .catch(permissionError);
+
+  testBtn.addEventListener('click', function () {
+
+  });
+
+
 });
